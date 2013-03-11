b6d6670326a5a174db7cc8db0120891f08292409
Hai-Nam Nguyen <hainam@jcisio.com>
2013-03-11 09:39:07 +0100

    Add drupalvietnam.org site

---
+++ www/sites/drupalvietnam.org/settings.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once __DIR__ . '/../default/default.settings.php';
+require_once __DIR__ . '/../../../conf/drupal/settings.php';
+
---
+++ www/sites/sites.php
@@ -0,0 +1,6 @@
+<?php
+
+$sites = array(
+  'localhost' => 'drupalvietnam.org',
+);
+
